2ce69a2d3643e1ccc298fa9e973071cc99b3c3e7
Merit Mohammed <cussmeritmohammed@gmail.com>
2026-01-30 20:38:31 +0100

    Adding the layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from "expo-router";
 
 //import icons
 import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabsLayout() {
     return(
@@ -9,21 +10,30 @@ export default function TabsLayout() {
             <Tabs.Screen 
                 name="home" 
                 options={{
-                    tabBarIcon: () => <Ionicons name="home-outline" size={24} color="black" />
+                    tabBarIcon: () => <Ionicons name="home-outline" size={24} color="black" />,
                 }}
             />
 
             <Tabs.Screen 
                 name="services" 
+                options={{
+                    tabBarIcon: () => <MaterialIcons name="dashboard" size={24} color="black" />
+                }}
             />
 
             <Tabs.Screen 
                 name="history" 
+                options={{
+                    tabBarIcon: () => <Ionicons name="time-outline" size={24} color="black" />
+                }}
             />
 
             <Tabs.Screen 
                 name="profile" 
-                />
+                 options={{
+                    tabBarIcon: () => <Ionicons name="person-outline" size={24} color="black" />
+                }}
+            />
         </Tabs>
     );
 }
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,15 +2,34 @@ import {
   Text, 
   View,
   StyleSheet,
+  Pressable,
+  KeyboardAvoidingView,
  }
 from "react-native";
 
 import { StatusBar } from "expo-status-bar";
 
+import { Link } from "expo-router";
+
 export default function HomeScreen() {
   return (
     <>
-    <View  style={styles.container}>
+    <View style={styles.container}>
+      <View style={styles.card}> 
+        {/* Section for text */}
+        <View>
+           <Text style={styles.title}> Wallet Balance </Text>
+           <Text style={styles.subTitle}> $2,4000.50 </Text>
+           <Text style={styles.dateTitle}> Last Updated: 19:25</Text>
+        </View>
+
+        {/* Add Money Buttonr */}
+        <Link href="/profile">
+          <Pressable style={styles.button}>
+              <Text style={styles.buttonText}> + Add Money </Text>
+          </Pressable>
+        </Link>
+      </View>
       <Text style={styles.textStyle}> THis is the home page </Text>
     </View> 
   </>
@@ -22,10 +41,46 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#29292e"
+    backgroundColor: "#fff",
   },
 
   textStyle: {
+    color: "black",
+  },
+
+  card: {
+    width: "90%",
+    paddingVertical: 35,
+    paddingHorizontal: 24,
+    backgroundColor: "#1a1a22",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+
+  title: {
     color: "#fff",
+    marginBottom: 30,
+  },
+
+  subTitle:{
+    color: "#fff",
+    marginBottom: 20,
+  },
+
+  dateTitle: {
+    color: "#fff",
+    marginBottom: 10,
+  },
+
+  button: {
+    width: "100%",
+    backgroundColor: "#fff",
+    paddingVertical: 15,
+    alignItems: "center",
+    borderRadius: 30,
+  },
+
+  buttonText: {
+    color: "black",
   },
 })
