6c6f330a3406de072c3d2ccfac232ec91b6b9ad7
Merit Mohammed <cussmeritmohammed@gmail.com>
2026-01-30 13:59:13 +0100

    added new file

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,27 @@
+import { 
+  Text, 
+  View,
+  StyleSheet,
+ }
+from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View  style={styles.container}>
+      <Text style={styles.textStyle}> Hello expo app </Text>
+    </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#29292e"
+  },
+
+  textStyle: {
+    color: "#fff",
+  },
+})
